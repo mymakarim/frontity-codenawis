c82d07ac557cdba417971ff5f4c13c0470534a47
ymakarim <ymakarim@gmail.com>
2020-07-04 19:44:35 +0430

    fonts added

--- packages/codenawis/src/components/index.js
+++ packages/codenawis/src/components/index.js
@@ -53,6 +53,7 @@ const Theme = ({ state }) => {
 export default connect(Theme);
 
 const globalStyles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap');
   body {
   margin: 0;
   font-family: "Poppins", Roboto,

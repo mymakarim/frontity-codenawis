541d4ec82bad819987b67cc7415a68066ab9811d
ymakarim <ymakarim@gmail.com>
2020-08-04 22:23:41 +0430

    The latest changes

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,9 +2,9 @@ const settings = {
   "name": "CodeNawis - Fashion Jackson",
   "state": {
     "frontity": {
-      "url": "https://componentity.com",
-      "title": "Componentity",
-      "description": "Thousands of Components Ready to be Copy-Pasted"
+      "url": "https://newschin.mymakarim.vercel.app",
+      "title": "CodeNawis",
+      "description": "A NextJs clone of FashionJackson.com"
     }
   },
   "packages": [
@@ -61,7 +61,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "api": "http://codenawis.com/componentity/wp-json"
+          "api": "https://fashionjackson.com/wp-json"
         }
       }
     },
